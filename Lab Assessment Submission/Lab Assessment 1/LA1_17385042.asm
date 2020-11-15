c8b22aebb619ec10aa0543c892ea58fa5a3079ea
1731185042 Hasan <nazim.hasan173@northsouth.edu>
2020-11-15 10:46:11 +0600

    La1 submitted

---
+++ Lab Assessment Submission/Lab Assessment 1/LA1_17385042.asm
@@ -0,0 +1,25 @@
+
+
+org 100h ; #include <stdio.h>    starting add - 0000, ending - FFFF;  prog start, assembler, return
+         ;02 -- 02H(){
+         
+            ;int main(){
+            
+            
+            MOV AX, 02
+            MOV BX, 03
+            MOV CX, 04
+            
+                    
+                    ;AX=BX+CX+DX        ;int a=2,  b= 3,  c=4;
+                    ;d=a+b+c;
+                    ;printf(d);
+                    ;return 0;
+
+
+
+ret
+
+
+
+
